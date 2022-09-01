a7df9c2b8f7e7af2be2fe240b4085950911bd4cf
pengwei <pengw.rppet.cn>
2022-09-01 17:18:01 +0800

    gprc配合token调用

--- hello/hello.pb.go
+++ hello/hello.pb.go
@@ -70,7 +70,7 @@ func init() {
 func init() { proto.RegisterFile("hello.proto", fileDescriptor_61ef911816e0a8ce) }
 
 var fileDescriptor_61ef911816e0a8ce = []byte{
-	// 179 bytes of a gzipped FileDescriptorProto
+	// 190 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0xce, 0x48, 0xcd, 0xc9,
 	0xc9, 0xd7, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x05, 0x73, 0x94, 0x14, 0xb9, 0x38, 0x83,
 	0x4b, 0x8a, 0x32, 0xf3, 0xd2, 0x5d, 0x4a, 0xf2, 0x85, 0x44, 0xb8, 0x58, 0xcb, 0x12, 0x73, 0x4a,
@@ -81,8 +81,8 @@ var fileDescriptor_61ef911816e0a8ce = []byte{
 	0xf1, 0xcf, 0x4b, 0x0d, 0x4f, 0xac, 0x24, 0x4e, 0xa3, 0x51, 0x21, 0x17, 0x6f, 0x40, 0x69, 0x52,
 	0x71, 0x69, 0x12, 0xcc, 0x9d, 0xba, 0x5c, 0xec, 0x01, 0xa5, 0x49, 0x39, 0x99, 0xc5, 0x19, 0x44,
 	0xba, 0x94, 0x33, 0xb8, 0x34, 0xa9, 0x38, 0xb9, 0x28, 0x33, 0x29, 0x95, 0x18, 0x0d, 0x06, 0x8c,
-	0x49, 0x6c, 0xe0, 0xb0, 0x34, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0xc8, 0xa9, 0xa1, 0x8f, 0x5a,
-	0x01, 0x00, 0x00,
+	0x46, 0x56, 0x5c, 0xdc, 0x8e, 0xa5, 0x25, 0x19, 0xe4, 0x04, 0x4c, 0x12, 0x1b, 0x38, 0x1e, 0x8c,
+	0x01, 0x01, 0x00, 0x00, 0xff, 0xff, 0x06, 0x1d, 0x7a, 0xb1, 0x96, 0x01, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -447,5 +447,79 @@ var _PubsubService_serviceDesc = grpc.ServiceDesc{
 			ServerStreams: true,
 		},
 	},
+	Metadata: "hello.proto",
+}
+
+// AuthServiceClient is the client API for AuthService service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type AuthServiceClient interface {
+	// 使用token验证
+	Hello(ctx context.Context, in *StringDto, opts ...grpc.CallOption) (*StringDto, error)
+}
+
+type authServiceClient struct {
+	cc *grpc.ClientConn
+}
+
+func NewAuthServiceClient(cc *grpc.ClientConn) AuthServiceClient {
+	return &authServiceClient{cc}
+}
+
+func (c *authServiceClient) Hello(ctx context.Context, in *StringDto, opts ...grpc.CallOption) (*StringDto, error) {
+	out := new(StringDto)
+	err := c.cc.Invoke(ctx, "/hello.AuthService/Hello", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// AuthServiceServer is the server API for AuthService service.
+type AuthServiceServer interface {
+	// 使用token验证
+	Hello(context.Context, *StringDto) (*StringDto, error)
+}
+
+// UnimplementedAuthServiceServer can be embedded to have forward compatible implementations.
+type UnimplementedAuthServiceServer struct {
+}
+
+func (*UnimplementedAuthServiceServer) Hello(ctx context.Context, req *StringDto) (*StringDto, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Hello not implemented")
+}
+
+func RegisterAuthServiceServer(s *grpc.Server, srv AuthServiceServer) {
+	s.RegisterService(&_AuthService_serviceDesc, srv)
+}
+
+func _AuthService_Hello_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(StringDto)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AuthServiceServer).Hello(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/hello.AuthService/Hello",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AuthServiceServer).Hello(ctx, req.(*StringDto))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _AuthService_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "hello.AuthService",
+	HandlerType: (*AuthServiceServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "Hello",
+			Handler:    _AuthService_Hello_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
 	Metadata: "hello.proto",
 }
